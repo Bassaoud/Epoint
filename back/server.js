7bf3adddaa2348a4047ced6dccec6c60e84f0fc0
Bassaoud <mohamed.bassaoud@yahoo.fr>
2023-04-16 00:50:02 +0200

    Ajout date au nom des fichiers

--- back/server.js
+++ back/server.js
@@ -36,7 +36,14 @@ const upload = multer({
       callback(null, '../uploads');
     },
     filename: (req, file, callback) => {
-      callback(null, `${Date.now()}-${file.originalname}`);
+      const now = new Date();
+      const day = now.getDate().toString().padStart(2, '0');
+      const month = (now.getMonth() + 1).toString().padStart(2, '0');
+      const year = now.getFullYear().toString().slice(-2);
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+
+      callback(null, `${day}${month}${year}${hours}${minutes}-${file.originalname}`);
     },
   }),
   fileFilter: (req, file, callback) => {
